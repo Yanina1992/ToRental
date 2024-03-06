cd164210605407990e8fbe4d138c67d6418db1dc
Yanina Agüero <132350660+Yanina1992@users.noreply.github.com>
2024-03-06 14:32:46 +0100

    datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ButtonComponent } from './components/button/button.component';
 import { HomeComponent } from './pages/home/home.component';
 import { VeicoliComponent } from './pages/veicoli/veicoli.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    ButtonComponent,
     HomeComponent,
-    VeicoliComponent
+    VeicoliComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+ ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/pages/veicoli/veicoli.component.ts
+++ src/app/pages/veicoli/veicoli.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { NgbCalendar, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+
+
+
 
 @Component({
   selector: 'app-veicoli',
@@ -6,5 +11,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./veicoli.component.scss']
 })
 export class VeicoliComponent {
+  dateControl = new FormControl();
+
+  /*codice da prova*/
+/*chat gpt*/
+model: NgbDateStruct | undefined;
+date: { year: number; month: number; } | undefined;
+
+isDatepickerOpen = false;
+
+constructor(private calendar: NgbCalendar) {}
+
+toggleDatePicker() {
+  this.isDatepickerOpen = !this.isDatepickerOpen;
+}
+
+selectToday() {
+  this.model = this.calendar.getToday();
+}
 
 }
