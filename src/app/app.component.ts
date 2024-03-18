901fe19f11a4c393d679771e0146141fbf951530
Yanina Agüero <132350660+Yanina1992@users.noreply.github.com>
2024-03-18 15:25:27 +0100

    Sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'tr';
+
+  color:boolean=false;
+
+ngAfterViewInit(): void {
+      this.getNavbarColor();
+}
+
+  constructor(){}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.getNavbarColor();
+  }
+
+  getNavbarColor(){
+    const navbar = document.querySelector('.navbar');
+    if(navbar){
+      const NavbarColor = window.getComputedStyle(navbar).backgroundColor;
+      if(NavbarColor== 'rgba(196, 4, 4, 0.98)'){
+        this.color= true;
+      }else{
+        this.color=false;
+      }
+    }
+  }
+
 }
