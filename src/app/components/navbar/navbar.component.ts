4ba8aff2e33bc500f0fbfe23831ddb2265d2a8cc
Yanina1992 <yaninaaguero@hotmail.it>
2024-04-08 11:54:19 +0100

    Fixing-navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +7,33 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  isMobileSize: boolean = false;
+  buttonClass: string = '';
+  divClass: string = '';
 
+  ngAfterViewInit():void{
+    this.changeAreaExpanded();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any){
+    this.changeAreaExpanded();
+  }
+
+  changeAreaExpanded(){
+    const button = document.getElementById('toggle-btn');
+    const toggleDiv = document.getElementById('navbarNavAltMarkup');
+
+    if(button){
+      if(window.innerWidth >= 576){
+        button.setAttribute('aria-expanded', 'true');
+        this.isMobileSize = true;
+        this.buttonClass = 'navbar-toggler d-none';
+        this.divClass = 'navbar-collapse collapse-show';
+      }else{
+        this.buttonClass = 'navbar-toggler-collapsed';
+        this.divClass = 'navbar-collapse collapse';
+      }
+    }
+  }
 }
